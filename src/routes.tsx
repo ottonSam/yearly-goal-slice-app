f4e4fc7a4ce569e727b1aa13fe6295ae2e19bcde
Samuel Otton <ottonsam@gmail.com>
2026-02-09 16:01:12 -0300

    feat: separate styleguide routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,68 +1,26 @@
 import { Navigate, createBrowserRouter } from "react-router-dom"
 
-import StyleguideLayout from "@/pages/styleguide/layout"
-import CheckboxPage from "@/pages/styleguide/components/checkbox/page"
-import DialogPage from "@/pages/styleguide/components/dialog/page"
-import DrawerPage from "@/pages/styleguide/components/drawer/page"
-import FieldPage from "@/pages/styleguide/components/field/page"
-import InputPage from "@/pages/styleguide/components/input/page"
-import ResponsiveDialogPage from "@/pages/styleguide/components/responsive-dialog/page"
-import ScrollAreaPage from "@/pages/styleguide/components/scroll-area/page"
-import SelectPage from "@/pages/styleguide/components/select/page"
-import TextareaPage from "@/pages/styleguide/components/textarea/page"
-import StyleguidePage from "@/pages/styleguide/page"
+import { styleguideRoutes } from "@/routes/styleguide"
+import RegisterPage from "@/pages/register/page"
+
+const isDev = import.meta.env.VITE_ENV === "dev"
+
+const baseRoutes = isDev
+  ? [
+      {
+        path: "/",
+        element: <Navigate to="/styleguide" replace />,
+      },
+    ]
+  : []
 
 const router = createBrowserRouter([
+  ...baseRoutes,
   {
-    path: "/",
-    element: <Navigate to="/styleguide" replace />,
+    path: "/register",
+    element: <RegisterPage />,
   },
-  {
-    path: "/styleguide",
-    element: <StyleguideLayout />,
-    children: [
-      {
-        index: true,
-        element: <StyleguidePage />,
-      },
-      {
-        path: "components/checkbox",
-        element: <CheckboxPage />,
-      },
-      {
-        path: "components/dialog",
-        element: <DialogPage />,
-      },
-      {
-        path: "components/drawer",
-        element: <DrawerPage />,
-      },
-      {
-        path: "components/field",
-        element: <FieldPage />,
-      },
-      {
-        path: "components/input",
-        element: <InputPage />,
-      },
-      {
-        path: "components/responsive-dialog",
-        element: <ResponsiveDialogPage />,
-      },
-      {
-        path: "components/select",
-        element: <SelectPage />,
-      },
-      {
-        path: "components/scroll-area",
-        element: <ScrollAreaPage />,
-      },
-      {
-        path: "components/textarea",
-        element: <TextareaPage />,
-      },
-    ],
-  },
+  ...(isDev ? styleguideRoutes : []),
 ])
 
 export default router
